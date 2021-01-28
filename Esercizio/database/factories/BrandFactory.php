ab83089a1de89644493fa9b0f9c3b46d2bad8c7b
Caterina <danizio@outlook.it>
2021-01-28 15:14:19 +0100

    Database brand

---
+++ Esercizio/database/factories/BrandFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Brand;
+use Faker\Generator as Faker;
+
+$factory->define(Brand::class, function (Faker $faker) {
+    return [
+        'name' => $faker ->name,
+        'logo' => $faker ->imageUrl($width = 640, $height = 480),
+        'n_locations' => $faker ->numberBetween($min = 1, $max = 100)
+    ];
+});
